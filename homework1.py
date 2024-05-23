f13ab3e063846496e11b4aee821e8af4ccaaec36
alekseyKos12 <Lexakosoy120486@gmail.com>
2024-05-23 20:05:38 +0800

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+example = 'обучение'
+print(example, example[0] , example[-1] , example[5:] , example[::-1] , example[1:8:2])
+print(example)
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:8:2])
