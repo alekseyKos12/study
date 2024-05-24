706aa73c378d92816ba0f33219a0e010f3f98e53
alekseyKos12 <Lexakosoy120486@gmail.com>
2024-05-24 17:45:41 +0800

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Course_name = "pyton"
+Number_of_hours_spent = 1.5
+Number_of_completed_DZ = 12
+Time_to_complete_a_single_task = 0.125
+print('курс', Course_name,',' 'всего задач', Number_of_completed_DZ,',' 'затраченно часов', Number_of_hours_spent, ',' 'среднее время выполнения', Time_to_complete_a_single_task)
