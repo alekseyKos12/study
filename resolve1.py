a00d4a80cc2c12d72f6d3bd3d0dd1c9e8550f62c
alekseyKos12 <Lexakosoy120486@gmail.com>
2024-06-08 16:32:08 +0800

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,23 @@
+a = ("арифметика")
+print(len(a))
+
+first = 15
+second = 10
+print(f"summa {first} and {second} = {first + second} , diff {first} and {second} = {15 - 10}")
+
+first = 10
+second = 15
+third = 20
+print(f" arithmetic mean {first} , {second} and {third} = {first + second + third // 3}")
+
+first_strong = "Вторник"
+third = "Понедельник"
+print(f"{third}, {first_strong}")
+
+a = 10
+b = 25
+c = 8
+print(f"f = {(a*b)+(a*c)**3//2} ")
+
+
+
